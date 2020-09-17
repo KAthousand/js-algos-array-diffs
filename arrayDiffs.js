c023058168c5ac235d751220940f5ba69629b04b
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-09-17 17:16:35 -0400

    Kyle Thousand

--- arrayDiffs.js
+++ arrayDiffs.js
@@ -1,6 +1,52 @@
 //ARRAY DIFFERENCES
 
-const arrayDiffs = (arr1, arr2) => {};
+// Pseudo
+// I need to create an empty array. Later I will push unique ints into it.
+// "let empty array"
+//
+// I need to push all unique int into the new array
+// I need to check for all unique integers.
+// I  need to check if any of the elements in arr1 are in arr2 and vice versa.
+//
+// "if the value of each index in arr1 equals the value of any index in arr2, do NOT push into the output array."
+//
+// "if the value of any index in arr1 does not equal the value of any index in arr2 push into the output array."
+//
+// 
+
+
+
+// const arrayDiffs = (arr1, arr2) => {
+
+//   let uniqueNums = [];
+
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (!arr2.includes(arr1[i])) {
+//       uniqueNums.push(arr1[i])
+//     }
+//   }
+//   for (let i = 0; i < arr2.length; i++) {
+//     if (!arr1.includes(arr2[i])) {
+//       uniqueNums.push(arr2[i])
+//     }
+//   }
+//   return uniqueNums;
+// }
+
+const arrayDiffs = (arr1, arr2) => {
+
+  let uniqueNums = [];
+
+  arr1.forEach((num) => {
+    arr2.includes(num) ? null : uniqueNums.push(num);
+  })
+
+  arr2.forEach((num) => {
+    arr1.includes(num) ? null : uniqueNums.push(num);
+  })
+
+  return uniqueNums;
+}
 
 console.log(arrayDiffs([1, 44, 2, 3, 5], [33, 1, 2, 3, 4, 5]));
 
